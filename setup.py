75795352584aebea1d5ad3acf9974da76ba73d04
qianjing2020 <qianjing2020@users.noreply.github.com>
2020-01-14 22:57:24 -0500

    updated version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name = "lambdata_qj",
-        version = "0.0.2",
+        version = "0.0.3",
         author = "qianjing",
         description="A collection of data science helper functions",
         long_description= LONG_DESCRIPTION,
