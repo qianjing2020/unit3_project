1c9f0ef687c63b4f9db059c8c022f98acc07baa1
qianjing2020 <qianjing2020@users.noreply.github.com>
2020-01-14 22:34:08 -0500

    python created distribution

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name = "lambdata_qj",
-        version = "0.0.1",
+        version = "0.0.2",
         author = "qianjing",
         description="A collection of data science helper functions",
         long_description= LONG_DESCRIPTION,
