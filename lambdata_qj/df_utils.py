bad183d6bbf8ab2c1d1097f0452e823f3318364c
qianjing2020 <qianjing2020@users.noreply.github.com>
2020-01-14 21:39:35 -0500

    added two functions in df_utils.py

--- lambdata_qj/df_utils.py
+++ lambdata_qj/df_utils.py
@@ -3,5 +3,26 @@ Utility functions for working with dataframes
 """
 
 import pandas as pd
+import numpy as np
+# generate random number 
+TEST_DF = pd.DataFrame([1,2,3,np.nan])
 
-TEST_DF = pd.DataFrame([1,2,3])
+def nan_finder(df):
+    """
+    input pandas dataframe
+    output rows and cols where the nans are
+    """ 
+    [(i,j) for i,j in zip(*np.where(df.isnull()))]
+    # to confirm nan
+    # [df.iloc[i,j] for i,j in zip(*np.where(z))]
+    
+nan_finder(TEST_DF)  
+
+
+def uniform_random(X1=0, X2=1, n=10):
+    """
+    input n, X1, X2, n must be integer
+    output n random numbers between X1,X2
+    """
+    x = np.random.uniform(X1, X2, n) 
+    return x
